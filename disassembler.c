f8256fe56e07ceb70053c77a119b753f8697c700
YukinoShary <shary490106898@gmail.com>
2022-05-16 00:48:41 +0900

    add the "add" and the "int" instructions

--- disassembler.c
+++ disassembler.c
@@ -25,16 +25,17 @@ typedef struct exec
 
 typedef struct instruction
 {
+    char seg_0[4];
     char seg_1[4];
-    char seg_2[4];
-    char seg_3[8];
     /*function code*/
     char reg[3];
     char w;
-    int mod[3];
-    int rm[2];
-    int data1[8];
-    int data2[8];
+    char d;
+    char mod[3];
+    char rm[2];
+    char data0[8];
+    char data1[8];
+    char type[8];
     char* asem[16];
     int length;
 } instruction;
@@ -60,16 +61,16 @@ int *buffer_ptr;
 
 void byte2binary(int decimal, char* binary_text);
 char* text_to_instruction(exec* hdr);
-void mov_i2r_interpreter(instruction *ins, char* binary_data);
+void mov_I2R_interpreter(instruction *ins, char* binary_data);
+void int_TS_interpreter(instruction *ins, char* binary_data);
+void add_RMR2E_interpreter(instruction *ins, char* binary_data);
 
 
 int main(int argc, char* argv[])
 {
-    size_t file_size;
     exec* hd;
     FILE* fp;
     int i;
-    unsigned char binary2long[4] = {0};
     fp = fopen(argv[1], "rb");
     read_buffer = malloc(16 * 1024);
     asem_result = malloc(sizeof(instructions_list));
@@ -78,13 +79,22 @@ int main(int argc, char* argv[])
     instruction_cache = malloc(1024);
     hd = malloc(sizeof(exec));
 
-    fread(&hd->a_magic[0], sizeof(unsigned char), 1, fp);
-    fread(&hd->a_magic[1], sizeof(unsigned char), 1, fp);
-    fread(&hd->a_flags, sizeof(unsigned char), 1, fp);
-    fread(&hd->a_cpu, sizeof(unsigned char), 1, fp);
-    fread(&(hd->a_hdrlen), sizeof(unsigned char), 1, fp);
-    fread(&hd->a_unused, sizeof(unsigned char), 1, fp);
-    fread(&hd->a_version, sizeof(short), 1, fp);
+    read_header();
+    text_to_instruction(hd);
+    free(read_buffer);
+}
+
+void read_header(exec* hdr, FILE* fp)
+{
+    size_t file_size;
+    unsigned char binary2long[4] = {0};
+    fread(&hdr->a_magic[0], sizeof(unsigned char), 1, fp);
+    fread(&hdr->a_magic[1], sizeof(unsigned char), 1, fp);
+    fread(&hdr->a_flags, sizeof(unsigned char), 1, fp);
+    fread(&hdr->a_cpu, sizeof(unsigned char), 1, fp);
+    fread(&(hdr->a_hdrlen), sizeof(unsigned char), 1, fp);
+    fread(&hdr->a_unused, sizeof(unsigned char), 1, fp);
+    fread(&hdr->a_version, sizeof(short), 1, fp);
 
     /*read a.out and put into buffer*/
     fseek(fp, 0L, SEEK_END);
@@ -99,7 +109,7 @@ int main(int argc, char* argv[])
         binary2long[i] = read_buffer[*buffer_ptr];
         *buffer_ptr ++;
     }
-    hd->a_text = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
+    hdr->a_text = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
         (binary2long[2] * 16 * 16 * 16 * 16) + (binary2long[1] * 16 * 16) + binary2long[0];
 
     for(i = 0; i < 4; i++)
@@ -107,7 +117,7 @@ int main(int argc, char* argv[])
         binary2long[i] = read_buffer[*buffer_ptr];
         *buffer_ptr ++;
     }
-    hd->a_data = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
+    hdr->a_data = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
         (binary2long[2] * 16 * 16 * 16 * 16) + (binary2long[1] * 16 * 16) + binary2long[0];
 
     for(i = 0; i < 4; i++)
@@ -115,7 +125,7 @@ int main(int argc, char* argv[])
         binary2long[i] = read_buffer[*buffer_ptr];
         *buffer_ptr ++;
     }
-    hd->a_bss = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
+    hdr->a_bss = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
         (binary2long[2] * 16 * 16 * 16 * 16) + (binary2long[1] * 16 * 16) + binary2long[0];
 
     for(i = 0; i < 4; i++)
@@ -123,7 +133,7 @@ int main(int argc, char* argv[])
         binary2long[i] = read_buffer[*buffer_ptr];
         *buffer_ptr ++;
     }
-    hd->a_entry = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
+    hdr->a_entry = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
         (binary2long[2] * 16 * 16 * 16 * 16) + (binary2long[1] * 16 * 16) + binary2long[0];
 
     for(i = 0; i < 4; i++)
@@ -131,7 +141,7 @@ int main(int argc, char* argv[])
         binary2long[i] = read_buffer[*buffer_ptr];
         *buffer_ptr ++;
     }
-    hd->a_total = (long)(binary2long[3] * 256 * 256 * 256) +
+    hdr->a_total = (long)(binary2long[3] * 256 * 256 * 256) +
         (binary2long[2] * 256 * 256) + (binary2long[1] * 256) + binary2long[0];
 
     for(i = 0; i < 4; i++)
@@ -139,28 +149,26 @@ int main(int argc, char* argv[])
         binary2long[i] = read_buffer[*buffer_ptr];
         *buffer_ptr ++;
     }
-    hd->a_syms = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
+    hdr->a_syms = (long)(binary2long[3] * 16 * 16 * 16 * 16 * 16 *16) +
         (binary2long[2] * 16 * 16 * 16 * 16) + (binary2long[1] * 16 * 16) + binary2long[0];
     
     *buffer_ptr = (int)hd->a_hdrlen;
     printf("a_hdrlen:%hhu\n", hd->a_hdrlen);
     printf("buffer pointer position:%d\n", *buffer_ptr);
     printf("text length:%ld\n", hd->a_text);
-    text_to_instruction(hd);
-    free(read_buffer);
 }
 
 /*byte to binary*/
-void byte2binary(int decimal, char* binary_text)
+void byte2binary(int decimal, char* binary_data)
 {
     int and_cul;
     and_cul = 255;
     for(int i = 0; i <= 7; i++)
     {
         if(and_cul & decimal == and_cul)
-            binary_text[i] = 1;
+            binary_data[i] = 1;
         else
-            binary_text[i] = 0;
+            binary_data[i] = 0;
     }
 }
 
@@ -202,20 +210,32 @@ char* text_to_instruction(exec* hdr)
         i = 0;
         while(i <= 3)
         {
-            ins->seg_1[i] = binary_data[i];
+            ins->seg_0[i] = binary_data[i];
             i ++;
         }
 
-        switch ((int)(ins->seg_1))
+        switch ((int)(ins->seg_0))
         {
             /*mov*/
-            case 1011:
+            case "1011":
                 mov_interpreter(ins, binary_data);
             break;
             /*int*/
-            case 1100:
-
+            case "1100": 
+                for(i = 4; i <= 7; i++)
+                {
+                    ins->seg_1[i] = binary_data[i];
+                }
+                if(ins->seg_1 == "1101")
+                    int_TS_interpreter(ins, binary_data);
                 break; 
+            case "0000":
+                for(i = 4; i <= 7; i++)
+                {
+                    ins->seg_1[i] = binary_data[i];
+                }
+                if(ins->seg_1[0] == '0' && ins->seg_1[1] == '0')
+                    add_RMR2E_interpreter(ins, binary_data);
             default:
             /*continue reading*/
         }
@@ -223,19 +243,19 @@ char* text_to_instruction(exec* hdr)
     
 }
 
-void mov_i2r_interpreter(instruction *ins, char* binary_data)
+void mov_I2R_interpreter(instruction *ins, char* binary_data)
 {
     int i, decimal;
     instruction_node* node;
-    node = malloc(sizeof(instruction_node)); 
-    for(i = 4; i <= 8; i++)
+    node = malloc(sizeof(instruction_node));
+    for(i = 4; i <= 7; i++)
     {
-        ins->seg_2[i] = binary_data[i];
+        ins->seg_1[i] = binary_data[i];
     }
-    ins->w = (int)ins->seg_2[0];
+    ins->w = (int)ins->seg_1[0];
     for(i = 1; i <= 3; i++)
     {
-        ins->reg[i - 1] = ins->seg_2[i];
+        ins->reg[i - 1] = ins->seg_1[i];
     }
 
     /*continue to read data*/
@@ -244,7 +264,7 @@ void mov_i2r_interpreter(instruction *ins, char* binary_data)
     byte2binary(decimal, binary_data);
     for(i = 0; i <= 7; i++)
     {
-        ins->data1[i] = binary_data[i];
+        ins->data0[i] = binary_data[i];
     }
     ins->length = 16;
     
@@ -255,12 +275,64 @@ void mov_i2r_interpreter(instruction *ins, char* binary_data)
         byte2binary(decimal, binary_data);
         for(i = 0; i <= 7; i++)
         {
-            ins->data2[i] = binary_data[i];
+            ins->data1[i] = binary_data[i];
         }
         ins->length = 24;
     }
+
+    /*TODO: set the asem*/
 
     node->ins = ins;
     list_add(node);
 }
 
+void int_TS_interpreter(instruction *ins, char* binary_data)
+{
+    int i, decimal;
+    instruction_node* node;
+    node = malloc(sizeof(instruction_node));
+    decimal = (int)read_buffer[*buffer_ptr];
+    *buffer_ptr ++;
+    byte2binary(decimal, binary_data)
+    for(i = 0; i <= 7; i++)
+    {
+        ins->type[i] = binary_data[i];
+    }
+    ins->length = 16;
+
+    /*TODO: set the asem*/
+
+    node->ins = ins;
+    list_add(node);
+}
+
+void add_RMR2E_interpreter(instruction *ins, char* binary_data)
+{
+    int i, decimal;
+    instruction_node* node;
+    node = malloc(sizeof(instruction_node));
+    ins->d = ins->seg_1[2];
+    ins->w = ins->seg_1[3];
+    decimal = (int)read_buffer[*buffer_ptr];
+    *buffer_ptr ++;
+    byte2binary(decimal, binary_data);
+
+    for(i = 0; i <= 2; i++)
+    {
+        ins->mod[i] = binary_data[i];
+    }
+    for(i = 3; i <= 5; i++)
+    {
+        ins->reg[i - 3] = binary_data[i];
+    }
+    for(i = 6; i <= 7; i++)
+    {
+        ins->rm[i - 6] = binary_data[i];
+    }
+    ins->length = 16;
+
+    /*TODO: set the asem*/
+
+    node->ins = ins;
+    list_add(node);
+}
